e3f50c1188d3ffe153a6322914d26641556e3272
Dogun123 <tpdnd7890@gmail.com>
2020-10-03 20:25:26 +0900

    fix_2

--- view/category.py
+++ view/category.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template,request
 
 menu_category = Blueprint('menu',__name__)
 
@@ -6,5 +6,14 @@ menu_category = Blueprint('menu',__name__)
 def menuhome():
     return render_template('menu.html')
 
+@menu_category.route('/menulist')
+def menulist():
+    school_name = request.args.get('school_name')
+    menus = []
+    for i in range(5):
+        menus.append("메뉴 {}".format(i))
+    print(school_name)
+    
+    return render_template('menulist.html',menus=menus,school_name=school_name)
 
 
